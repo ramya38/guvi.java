cfc8331ce0f73d0c7b1327c71536241710c71b65
ramya38 <sramya2309@gmail.com>
2017-07-10 10:04:34 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class natural
+{
+	public static void main(String args[])
+	{
+		int sum=0;
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		for(int i=1;i<=a;i++)
+	sum=sum+i;
+	System.out.print(sum);
+	}
+}
