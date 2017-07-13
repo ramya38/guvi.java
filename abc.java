9eec2ce7fb827e8396322c7c493db30385f338c7
ramya38 <sramya2309@gmail.com>
2017-07-13 11:48:24 +0530

    Create abc.java

---
+++ abc.java
@@ -0,0 +1,11 @@
+import java.*;
+import java.util.*;
+class abc
+{
+	public static void main(String args[])
+	{
+		StringBuffer s=new StringBuffer("world");
+		s.reverse();
+		System.out.print(s);
+	}
+}
